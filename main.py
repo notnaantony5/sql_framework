a0a95790b783aa6358158e8bf559505b295dead4
Your Name <you@example.com>
2024-03-11 20:17:14 +0300

    init

--- main.py
+++ main.py
@@ -1,81 +1,16 @@
-from framework import Model, String, Integer, run
-
-
-class Person(Model):
-    name: String
-    age: Integer
-
-
-class Salary(Model):
-    salary: Integer
-    person: Integer
-
-
-# run()
-# Person.create(name='John', age=4)
-
-persons = [{
-    'name': 'John',
-    'age': 14
-},
-    {
-        'name': 'Andrew',
-        'age': 22
-    }]
-
-
-class AgeFilter:
-    def __init__(self, start_age=0, end_age=None, key=None):
-        self.key = key
-        self.start_age = start_age
-        self.end_age = end_age
-
-    def __call__(self, age: int) -> bool:
-        if self.key is not None:
-            age = self.key(age)
-        if age < self.start_age:
-            return False
-        if self.end_age is not None:
-            if age > self.end_age:
-                return False
-        return True
-
-
-print(list(
-    filter(AgeFilter(start_age=18, key=lambda x: x['age']), persons)
-))
-print(list(
-    filter(AgeFilter(end_age=18, key=lambda x: x['age']), persons)
-))
-
-class NameFilter:
-    def __init__(self, letter):
-        self.letter = letter.lower()
-
-
-
-    def __call__(self, name: str) -> bool:
-        return self.letter in name.lower()
-
-names = ['Антон', 'Евгений', 'Алиса']
-
-print(
-    list(
-        filter(NameFilter('лиса'), names)
-    )
-)
-
-class Human:
-    def __init__(self, name):
-        self.name = name
-        self.зззз = 4
-
-    def __call__(self):
-        print(self.name)
-
-h1 = Human('Саша')
-h2 = Human('Егор')
-h1()
-h2()
-
-print(h1.зззз)
+lst = [1, 2, 3, 4, 5]
+gen = ({
+    i: i * i
+    for i in lst
+    if not i % 2})
+names = ['John', 'Smith', 'Alice', 'Victor', 'David']
+dct = {name: len(name)
+       for name in names
+       if 'a' not in name.lower()
+       and 'r' not in name.lower()}
+"""
+Нужно собрать при помощи словарного включения словарь
+В котором ключ - имя, значение - количество символов 
+в имени при условии что берем имена в которых нет букв
+'A' и 'R' в обоих регистрах { 'John': 4, 'Smith': 5 }
+"""
